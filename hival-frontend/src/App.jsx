ec30131946bd8e5263610f6345a75427828da906
hugopazr <hrpaz@unicesar.edu.co>
2026-02-06 19:30:46 -0500

    Creacion del Frontend

---
+++ hival-frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-400">
+        Hival Tech and Home 🚀
+      </h1>
+    </div>
+  )
+}
+
